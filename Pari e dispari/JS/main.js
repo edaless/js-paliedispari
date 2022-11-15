24a28c2b941dc7e2275bee69a8b02516672ee689
Emanuele D'Alessandro <dalessandroemanuelek@gmail.com>
2022-11-15 17:42:37 +0100

    pari e dispari con prompts

--- Pari e dispari/JS/main.js
+++ Pari e dispari/JS/main.js
@@ -1,3 +1,58 @@
+// Pari e Dispari
+// L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+// Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+// Sommiamo i due numeri
+// Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+// Dichiariamo chi ha vinto.
+
+function randomMinMax (min, max){
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function pariODispari (numero){
+    if(numero % 2 === 0){
+        return "pari";
+    }else{
+        return "dispari";
+    };
+}
+
+const min = 1;
+const max = 5;
+
+
+
+
+const squadraUser = prompt("pari o dispari?");
+
+console.log("squadraUser:  ", squadraUser)
+
+
+const numUtente = parseInt(prompt("Dimmi un numero da " + min + " a " + max));
+console.log("numUtente:  ", numUtente)
+const numPc = randomMinMax (min, max);
+console.log("numPc:  ", numPc)
+
+const somma = numPc + numUtente;
+console.log("somma:  ", somma)
+
+
+if (squadraUser === pariODispari(somma)){
+    console.log("hai vinto!");
+}else{
+    console.log("hai perso!");
+};
+
+
+
+
+
+
+
+
+
+
+
 
 
 
