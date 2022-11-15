aa780ca17e3079d0c1322b3a0e946a39cf8024c9
Emanuele D'Alessandro <dalessandroemanuelek@gmail.com>
2022-11-15 16:55:10 +0100

    pali

--- Palindroma/JS/main.js
+++ Palindroma/JS/main.js
@@ -1,3 +1,44 @@
+// Palidroma
+// Chiedere all’utente di inserire una parola
+// Creare una funzione per capire se la parola inserita è palindroma
+// Dare output relativo.
+
+
+
+
+
+function inverso(campione){
+    let strInversa = "";
+    // ciclo che genera la parola inversa
+    for (let i = (campione.length - 1); i >= 0; i--){
+        strInversa += campione[i];
+    }
+    // console.log(strInversa);
+    return strInversa;
+}
+
+function palindromità(campione){
+    if (campione === inverso(campione)){
+        return true;
+    }else{
+        return false;
+    };
+}
+
+
+const parola = prompt("dimmi una parola");
+
+if(palindromità(parola)){
+    console.log("La parola '" + parola + "' è PALINDROMA");
+}else{
+    console.log("La parola '" + parola + "' NON è PALINDROMA");
+};
+
+
+
+
+
+
 
 
 
